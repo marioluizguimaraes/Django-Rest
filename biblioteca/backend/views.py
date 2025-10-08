18611a6fb76db8483373d26cb5ebc13fafb5f894
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-07 23:56:01 -0300

    builder: configurando as views para acessar os models

--- biblioteca/backend/views.py
+++ biblioteca/backend/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Livro, Categoria, Editora, Autor, Usuario, Emprestimo, Multa, Reserva
+from .serializers import LivroSerializer, CategoriaSerializer, EditoraSerializer, AutorSerializer, UsuarioSerializer, EmprestimoSerializer, MultaSerializer, ReservaSerializer
 
-# Create your views here.
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer 
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaViewSet 
+
+class EditoraViewSet(viewsets.ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraViewSet 
+
+
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer 
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer 
+
+
+
+class EmprestimoViewSet(viewsets.ModelViewSet):
+    queryset = Emprestimo.objects.all()
+    serializer_class = EmprestimoSerializer 
+
+class MultaViewSet(viewsets.ModelViewSet):
+    queryset = Multa.objects.all()
+    serializer_class = MultaSerializer 
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer 
+
