815d665d81017609c2640063703b756c02dff2be
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-08 00:16:39 -0300

    fix: corrigindo erro de declaração das classes de serializers

--- biblioteca/backend/views.py
+++ biblioteca/backend/views.py
@@ -1,41 +1,46 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Livro, Categoria, Editora, Autor, Usuario, Emprestimo, Multa, Reserva
-from .serializers import LivroSerializer, CategoriaSerializer, EditoraSerializer, AutorSerializer, UsuarioSerializer, EmprestimoSerializer, MultaSerializer, ReservaSerializer
+from .serializers import (
+    LivroSerializer, CategoriaSerializer, EditoraSerializer, 
+    AutorSerializer, UsuarioSerializer, EmprestimoSerializer, 
+    MultaSerializer, ReservaSerializer
+)
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer 
+    serializer_class = LivroSerializer
+
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
-    serializer_class = CategoriaViewSet 
+    serializer_class = CategoriaSerializer
+
 
 class EditoraViewSet(viewsets.ModelViewSet):
     queryset = Editora.objects.all()
-    serializer_class = EditoraViewSet 
-
+    serializer_class = EditoraSerializer
 
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
-    serializer_class = AutorSerializer 
+    serializer_class = AutorSerializer
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer 
-
+    serializer_class = UsuarioSerializer
 
 
 class EmprestimoViewSet(viewsets.ModelViewSet):
     queryset = Emprestimo.objects.all()
-    serializer_class = EmprestimoSerializer 
+    serializer_class = EmprestimoSerializer
+
 
 class MultaViewSet(viewsets.ModelViewSet):
     queryset = Multa.objects.all()
-    serializer_class = MultaSerializer 
+    serializer_class = MultaSerializer
+
 
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
-    serializer_class = ReservaSerializer 
-
+    serializer_class = ReservaSerializer
