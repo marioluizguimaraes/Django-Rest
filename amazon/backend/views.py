a34f551705eec78c1cc9ce10568edb1209559af2
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-07 22:35:37 -0300

    feat: configurando as rotas

--- amazon/backend/views.py
+++ amazon/backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cliente
+from .serializers import ClienteSerializer
 
-# Create your views here.
+#O ModelViewSet fornece ações CRUD (Create, Retrieve, Update, Delete) para um modelo específico. 
+class ClienteViewSet(viewsets.ModelViewSet):
+    #O queryset define a consulta que será usada para recuperar os objetos do banco de dados.
+    queryset = Cliente.objects.all()
+    
+    #O serializer_class define o serializer que será usado para serializar os objetos recuperados do banco de dados.
+    serializer_class = ClienteSerializer 
