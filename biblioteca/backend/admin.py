62ea4229da38c7612e4ecc9c72b92780c67c4ed3
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-08 00:14:46 -0300

    buider: implemantado models para o admin do django

--- biblioteca/backend/admin.py
+++ biblioteca/backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Livro, Categoria, Editora, Autor, Usuario, Emprestimo, Multa, Reserva
 
-# Register your models here.
+admin.site.register(Livro)
+admin.site.register(Categoria)
+admin.site.register(Editora)
+
+admin.site.register(Autor)
+admin.site.register(Usuario)
+
+admin.site.register(Emprestimo)
+admin.site.register(Multa)
+admin.site.register(Reserva)
