e35d1dd630f9180c8e8de05d1b84ed6b60ac0099
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-07 23:22:33 -0300

    .

--- amazon/backend/views.py
+++ amazon/backend/views.py
@@ -3,10 +3,10 @@ from rest_framework import viewsets
 from .models import Cliente
 from .serializers import ClienteSerializer
 
-#O ModelViewSet fornece ações CRUD (Create, Retrieve, Update, Delete) para um modelo específico. 
+# O ModelViewSet fornece ações CRUD (Create, Retrieve, Update, Delete) para um modelo específico. 
 class ClienteViewSet(viewsets.ModelViewSet):
-    #O queryset define a consulta que será usada para recuperar os objetos do banco de dados.
+    # O queryset define a consulta que será usada para recuperar os objetos do banco de dados.
     queryset = Cliente.objects.all()
     
-    #O serializer_class define o serializer que será usado para serializar os objetos recuperados do banco de dados.
+    # O serializer_class define o serializer que será usado para serializar os objetos recuperados do banco de dados.
     serializer_class = ClienteSerializer 
