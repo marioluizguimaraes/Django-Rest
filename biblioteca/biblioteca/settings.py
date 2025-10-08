36237378eaf359d1c7ba1fe30a0a8e783c5cb102
mario.guimaraes <mariolsg.oficial@gmail.com>
2025-10-07 23:35:21 -0300

    builder: configurando settings do projeto

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -15,6 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -48,8 +50,12 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'biblioteca',
+        'USER': 'postgres',
+        'PASSWORD': '1334425',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
